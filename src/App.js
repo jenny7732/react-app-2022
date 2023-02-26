54999547058d7a92a1f5dce02eb304fe9dcc9398
Yujin Lee <jenny7732@naver.com>
2023-02-26 17:25:39 +0900

    feat.CRUD 간단한 기능 완성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,160 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
+function Article(props) {
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+function Header(props) {
+  return <header>
+    <h1><a href="/" onClick={(event) => {
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
+  </header>
+}
+function Nav(props) {
+  const lis = []
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/' + t.id} onClick={event => {
+        event.preventDefault();
+        props.onChangeMode(Number(event.target.id));
+      }}>{t.title}</a>
+    </li>)
+  }
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
+}
+function Create(props) {
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event => {
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title" /></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>
+}
+function Update(props) {
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+
+  return <article>
+    <h2>Update</h2>
+    <form onSubmit={event => {
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onUpdate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title" value={title} onChange={e => {
+        setTitle(e.target.value);
+      }} /></p>
+      <p><textarea name="body" placeholder="body" value={body} onChange={e => {
+        setBody(e.target.value);
+      }}></textarea></p>
+      <p><input type="submit" value="Update"></input></p>
+    </form>
+  </article>
+}
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+    { id: 3, title: 'javascript', body: 'javascript is ...' }
+  ]);
+  let content = null;
+  let contextControl = null;
+  if (mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  } else if (mode === 'READ') {
+
+    // eslint-disable-next-line array-callback-return
+    content = topics.map(props => {
+      if (props.id === id) {
+        return <Article key={props.id} title={props.title} body={props.body}></Article>
+      }
+    })
+    contextControl = <>
+      <li><a href={"/update/" + id} onClick={e => {
+        e.preventDefault();
+        setMode('UPDATE');
+      }}>Update</a></li>
+      <li><input type="button" value="Delete" onClick={() => {
+        const newTopics = [];
+        topics.map(props => {
+          if (props.id !== id) {
+            newTopics.push(props);
+          }
+        })
+        setTopics(newTopics);
+        setMode('WELCOME')
+      }} /></li>
+    </>
+  } else if (mode === 'CREATE') {
+    content = <Create onCreate={(_title, _body) => {
+      const newTopic = { id: nextId, title: _title, body: _body }
+      const newTopics = [...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId + 1);
+    }}></Create>
+  } else if (mode === "UPDATE") {
+    let title, body = null;
+    // eslint-disable-next-line array-callback-return
+    topics.map(props => {
+      if (props.id === id) {
+        title = props.title;
+        body = props.body;
+      }
+    })
+    content = <Update title={title} body={body} onUpdate={(title, body) => {
+      const newTopics = [...topics];
+      const UpdatedTopic = { id: id, title: title, body: body };
+      // eslint-disable-next-line array-callback-return
+      newTopics.map(props => {
+        if (props.id === id) {
+          props.title = UpdatedTopic.title;
+          props.body = UpdatedTopic.body;
+        }
+      })
+      setTopics(newTopics);
+      setMode('READ');
+    }}></Update>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="WEB" onChangeMode={() => {
+        setMode('WELCOME');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(_id) => {
+        setMode('READ');
+        setId(_id);
+      }}></Nav>
+      {content}
+      <ul>
+        <li><a href="/create" onClick={event => {
+          event.preventDefault();
+          setMode('CREATE');
+        }}>Create</a></li>
+        {contextControl}
+      </ul>
     </div>
   );
 }
